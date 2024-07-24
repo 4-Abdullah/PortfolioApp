f821d4455b6fef98744f66914bd1feca4ec0c83e
4-Abdullah <abdullahmaliksher@gmail.com>
2024-07-24 19:54:16 +0500

    Card style updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,32 @@
-import { Heading, HStack, Image, TagLeftIcon, Text, VStack } from "@chakra-ui/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+// import { Heading, HStack, Image, TagLeftIcon, Text, VStack } from "@chakra-ui/react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+// import React from "react";
 
-const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return (
+// const Card = ({ title, description, imageSrc }) => {
+//   // Implement the UI for the Card component according to the instructions.
+//   // You should be able to implement the component with the elements imported above.
+//   // Feel free to import other UI components from Chakra UI if you wish to.
+//   return (
   
-    <HStack >
-      <VStack  className="Card_style">
-        <Image src={imageSrc} border-radius="10px"
-    width="100%" />
-        <div className='div_style'> 
-        <Heading size='md' p={'5px'} >{title}</Heading>
-        <Text className="text_style" p={'5px'}>{description}</Text>
-        <HStack  p={'5px'}>
-            <div className="veiw_more">See more</div>
-            <FontAwesomeIcon icon={faArrowRight} size="1x"/>
-        </HStack>
-        </div>
-      </VStack>
-    </HStack>
+//     <HStack >
+//       <VStack  className="Card_style">
+//         <Image src={imageSrc} border-radius="10px"
+//     width="100%" />
+//         <div className='div_style'> 
+//         <Heading size='md' p={'5px'} >{title}</Heading>
+//         <Text className="text_style" p={'5px'}>{description}</Text>
+//         <HStack  p={'5px'}>
+//             <div className="veiw_more">See more</div>
+//             <FontAwesomeIcon icon={faArrowRight} size="1x"/>
+//         </HStack>
+//         </div>
+//       </VStack>
+//     </HStack>
 
-  );
-};
-export default Card;
+//   );
+// };
+// export default Card;
 
 // import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react"; 
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
@@ -61,3 +61,37 @@ export default Card;
 // }; 
  
 // export default Card; 
+
+import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const Card = ({ title, description, imageSrc }) => {
+  return (
+    <HStack
+      spacing={4}
+      p={5}
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      boxShadow="md"
+      _hover={{ boxShadow: "lg" }}
+      className="Card_style"
+    >
+      <Image src={imageSrc} borderRadius="lg" width="40%" />
+      <VStack align="start" spacing={3} width="60%">
+        <Heading size="md">{title}</Heading>
+        <Text>{description}</Text>
+        <HStack spacing={2} align="center">
+          <Text color="blue.500" cursor="pointer">
+            See more
+          </Text>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" color="blue.500" />
+        </HStack>
+      </VStack>
+    </HStack>
+  );
+};
+
+export default Card;
