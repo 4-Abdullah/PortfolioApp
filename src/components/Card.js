19765d622607a324b1ae0829b34a2dd621d172d5
4-Abdullah <abdullahmaliksher@gmail.com>
2024-07-25 10:58:47 +0500

    Only LandingSection Run

--- src/components/Card.js
+++ src/components/Card.js
@@ -37,11 +37,10 @@ const Card = ({ title, description, imageSrc }) => {
    return ( 
      <VStack 
        color="black" 
-       backgroundColor="white" 
-       cursor="pointer" 
+       backgroundColor="white"  
        borderRadius="xl" 
      > 
-       <Image borderRadius="xl" src={imageSrc} alt={title} /> 
+       <Image borderRadius="xl" src={imageSrc} width='100%' alt={title} /> 
        <VStack spacing={4} p={4} alignItems="flex-start"> 
          <HStack justifyContent="space-between" alignItems="center"> 
            <Heading as="h3" size="md"> 
@@ -53,7 +52,7 @@ const Card = ({ title, description, imageSrc }) => {
          </Text> 
          <HStack spacing={2} alignItems="center"> 
            <p>See more</p> 
-           <FontAwesomeIcon icon={faArrowRight} size="1x" /> 
+           <FontAwesomeIcon icon={faArrowRight}   size="1x" /> 
          </HStack> 
        </VStack> 
      </VStack> 
@@ -61,37 +60,3 @@ const Card = ({ title, description, imageSrc }) => {
 }; 
  
 export default Card; 
-
-// import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-// import React from "react";
-
-// const Card = ({ title, description, imageSrc }) => {
-//   return (
-//     <HStack
-//       spacing={4}
-//       p={5}
-//       borderWidth="1px"
-//       borderRadius="lg"
-//       overflow="hidden"
-//       boxShadow="md"
-//       _hover={{ boxShadow: "lg" }}
-//       className="Card_style"
-//     >
-//       <Image src={imageSrc} borderRadius="lg" width="40%" />
-//       <VStack align="start" spacing={3} width="60%">
-//         <Heading size="md">{title}</Heading>
-//         <Text>{description}</Text>
-//         <HStack spacing={2} align="center">
-//           <Text color="blue.500" cursor="pointer">
-//             See more
-//           </Text>
-//           <FontAwesomeIcon icon={faArrowRight} size="1x" color="blue.500" />
-//         </HStack>
-//       </VStack>
-//     </HStack>
-//   );
-// };
-
-// export default Card;
