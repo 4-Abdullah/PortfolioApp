4c9a404a8a5f51fd3dd76d05064bcf001ea1444b
4-Abdullah <abdullahmaliksher@gmail.com>
2024-12-29 14:21:56 +0500

    Update LandingSection component with new greeting and local avatar image

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
-
-const greeting = "Hello, I am Pete!";
+import mypic from '../images/mypic.png'
+const greeting = "Hello, I am Abdullah!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -15,15 +15,11 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-    {/* <img src='https://i.pravatar.cc/150?img=7' alt='Avatar'  className="img_style"/>
-    <p>{greeting}</p>
-    <h1>{bio1}</h1>
-    <h2>{bio2}</h2> */}
    
    <VStack spacing={16}> 
      <VStack spacing={4} alignItems="center"> 
        <Avatar 
-         src="https://i.pravatar.cc/150?img=7" 
+         src={mypic} 
          size="2xl" 
          name="Your Name" 
        /> 
